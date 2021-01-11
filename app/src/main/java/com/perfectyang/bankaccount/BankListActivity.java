9cffab0a383a998c4994dde7dc14f7cb3fe9142e
perfectyang <2406790623@qq.com>
2021-01-11 22:08:42 +0800

    添加数据

--- app/src/main/java/com/perfectyang/bankaccount/BankListActivity.java
+++ app/src/main/java/com/perfectyang/bankaccount/BankListActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,6 +19,8 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.perfectyang.bankaccount.mydb.Mydatabase;
 
 import org.json.JSONObject;
 
@@ -29,28 +34,30 @@ public class BankListActivity extends AppCompatActivity {
     private ArrayList<String> data = new ArrayList<>();
     private ListView listvw;
     ClipboardManager clipboard;
+    private SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_list);
-//        copy = findViewById(R.id.copy);
-//        tvw = findViewById(R.id.content);
-
-//        copy.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-//                //创建ClipData对象
-//                //第一个参数只是一个标记，随便传入。
-//                //第二个参数是要复制到剪贴版的内容
-//                ClipData clip = ClipData.newPlainText("mydata", tvw.getText().toString());
-//                //传入clipdata对象.
-//                clipboard.setPrimaryClip(clip);
-//                Toast.makeText(BankListActivity.this, "复制成功", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        initDatabase();
+        getAccountList();
         initView();
+    }
+
+    private void initDatabase() {
+        Mydatabase mydatabase = new Mydatabase(BankListActivity.this);
+        database = mydatabase.getWritableDatabase();
+    }
+
+    private void getAccountList () {
+        String sql = "select * from user";
+        Cursor cursor = database.rawQuery(sql, new String[]{});
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex("username"));
+            String pwd = cursor.getString(cursor.getColumnIndex("password"));
+            data.add(name + "=" + pwd);
+        }
     }
 
     private void setCopy (String content) {
@@ -90,11 +97,6 @@ public class BankListActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        data.add("在这里1");
-        data.add("在这里2");
-        data.add("在这里3");
-        data.add("在这里4");
-        data.add("在这里5");
         listvw = findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
         listvw.setAdapter(adapter);
@@ -102,8 +104,20 @@ public class BankListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String str = data.get(position);
-                setCopy(str);
+
+                String[] list = str.split("=");
+//                setCopy(str);
+                int i = deletUser(list[0], list[1]);
+                data.remove(position);
+                adapter.notifyDataSetChanged();
             }
         });
     }
+
+    public int deletUser(String username, String password){
+        int i = database.delete("user", "username=? and password=?", new String[]{username, password});
+        return i;
+    }
+
+
 }
